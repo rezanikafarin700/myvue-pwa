db82d5f9ef6ad4a70d6c09e296ebe930df962104
rezanikafarin <rezanikafarin100@gmail.com>
2021-09-18 17:04:09 +0430

    init git

---
+++ precache-manifest.10aae2b36afc87380644242c2f7a55c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d5055d5b276bf6e659",
+    "url": "/css/app.20157cd0.css"
+  },
+  {
+    "revision": "35acfe3df3921a1517b0",
+    "url": "/css/chunk-6db82968.392e784f.css"
+  },
+  {
+    "revision": "fdced4b3c42d93a3e77b9ee2d1954210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d5055d5b276bf6e659",
+    "url": "/js/app.f37fb3ac.js"
+  },
+  {
+    "revision": "35acfe3df3921a1517b0",
+    "url": "/js/chunk-6db82968.6411a005.js"
+  },
+  {
+    "revision": "c3d2292d4354b9ad406c",
+    "url": "/js/chunk-vendors.ce551d17.js"
+  },
+  {
+    "revision": "e49034e84490e8bee5826512d54ec775",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
